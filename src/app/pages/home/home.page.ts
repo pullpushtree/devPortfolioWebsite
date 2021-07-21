f053d9fb85a61232721919cbb6dbea66cabe1064
pullpushtree <bad7legend@gmail.com>
2021-07-20 22:12:27 -0400

    Added home page

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,44 @@
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+  glowEffect = false;
+  @ViewChild('caseBtn', { read: ElementRef } ) caseBtn: ElementRef;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  hoverEffect(event){
+    const xTouch = event.clientX;
+    const yTouch = event.clientY;
+
+    const rect = this.caseBtn.nativeElement.getBoundingClientRect();
+    const topBoundary = rect.top + 2;
+    const leftBoundry = rect.left + 2;
+    const rightBoundary = rect.right - 2;
+
+
+    if ( xTouch < leftBoundry || xTouch > rightBoundary || yTouch < topBoundary ) {
+      this.glowEffect = false;
+    }
+  }
+
+  linkedInSite(){
+
+  }
+
+  githubSite(){
+
+  }
+
+  behnaceSite(){
+
+  }
+
+}
