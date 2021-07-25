6684b08ce0c7e05e67b14a5eff1aefe9324619bc
pullpushtree <bad7legend@gmail.com>
2021-07-24 22:00:43 -0400

    Added save message to firebase,

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.page.html',
@@ -7,31 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPage implements OnInit {
   emailAddress: string;
-  fname: string;  
+  fname: string;
   body: string;
   submitted: boolean;
 
   constructor(
+    public afDb: AngularFireDatabase,
+    public afs: AngularFirestore,
+    public toastCtrl: ToastController
   ) {}
 
-  onSubmit() {
-    const message = `My name is ${this.fname} 
-      and email is ${this.emailAddress}
-      and this is my message ${this.body}
-      `;
-    alert(message);
+  ngOnInit() {}
 
+  onSubmit() {
     this.submitted = true;
     let msgPayload = {
       name: this.fname,
       email: this.emailAddress,
-      isHtml: false,      
-      body: this.body
+      body: this.body,
+      isHtml: false,
     };
 
     console.log('msgPayload value ', msgPayload);
 
+    this.afs
+      .collection('messages')
+      .add(msgPayload)
+      .then((res) => {
+        this.messageSuccess();
+      })
+      .catch((err) => {
+        this.messageFailed();
+      });
   }
 
-  ngOnInit() {}
+  async messageSuccess(){
+    await this.toastCtrl.create({
+      message: "Thank You!, Your message has been sent",
+      duration: 3000,
+      position: 'bottom',
+      color: 'warning'
+
+    }).then(res => res.present());
+  }
+
+  async messageFailed(){
+    await this.toastCtrl.create({
+      message: "Something went wrong. Please try Again!",
+      duration: 3000,
+      position: 'bottom',
+      color: 'danger'
+
+    }).then(res => res.present());
+  }
+
 }
