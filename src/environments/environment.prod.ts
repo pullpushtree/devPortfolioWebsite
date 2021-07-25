5a0942f11ab2c75b910452065abbd5c1e6496830
pullpushtree <bad7legend@gmail.com>
2021-07-25 13:54:58 -0400

    Added environment prod values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  hostURL: 'http://localhost:5000/',
+  envName: 'prod',  
+  firebaseConfig: {
+    apiKey: "AIzaSyCEocL3ubYqmlk-CFiAi71Y5r5GElH6jnY",
+    authDomain: "devportfoliowebsite.firebaseapp.com",
+    projectId: "devportfoliowebsite",
+    storageBucket: "devportfoliowebsite.appspot.com",
+    messagingSenderId: "113915178274",
+    appId: "1:113915178274:web:4a144b5923d28309e3c97b",
+    measurementId: "G-M04XCDY6L8"
+  }  
 };
